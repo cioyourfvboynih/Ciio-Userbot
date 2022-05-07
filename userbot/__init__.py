d6b1307b80cace13b7243dba800ea726c9bf207a
KingCio <103745710+cioyourfvboynih@users.noreply.github.com>
2022-05-07 10:06:31 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -101,6 +101,7 @@ DEVS = (
     1780709155, #Cio
     2055816454, #Dato
     1731365899, #Brian
+    5240722129, #choki
 )
 
 SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "1780709155").split()}
